c448c92681f81911c38a4e1ef9aaf67ce0e02e73
Verin1005 <daqingchong0809@gmail.com>
2024-06-05 19:02:25 +0800

    fmt

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 ![Logo](https://avatars.githubusercontent.com/u/51339301?s=200&v=4)
 
-## Litentry Dynamic Assertion Contracts 
+## Litentry Dynamic Assertion Contracts
+
 This repo is the contracts part separated from the [litentry-parachain](https://github.com/litentry/litentry-parachain).
 
 ## System Requirements
 
-- Node.js
-- Typescript
-- [Hardhat](https://hardhat.org/hardhat-runner/docs/getting-started#overview)
-- Solidity
+-   Node.js
+-   Typescript
+-   [Hardhat](https://hardhat.org/hardhat-runner/docs/getting-started#overview)
+-   Solidity
 
 ## Configuration
 
-- [Solidity versions](https://hardhat.org/hardhat-runner/docs/advanced/multiple-solidity-versions)
-- [Configuration Variables](https://hardhat.org/hardhat-runner/docs/config#configuration)
+-   [Solidity versions](https://hardhat.org/hardhat-runner/docs/advanced/multiple-solidity-versions)
+-   [Configuration Variables](https://hardhat.org/hardhat-runner/docs/config#configuration)
 
 ## Getting started
 
@@ -23,34 +24,32 @@ This repo is the contracts part separated from the [litentry-parachain](https://
 
 3. Switch to the right Node.js version using NVM.
 
-   ```typescript
-   # From the repo's root
-   nvm use
-   ```
+    ```typescript
+    # From the repo's root
+    nvm use
+    ```
 
 4. Install dependencies with `npm`
 
-   ```typescript
-   # From the repo's root
-   npm install
-   ```
+    ```typescript
+    # From the repo's root
+    npm install
+    ```
 
 5. Compile contracts
 
-   ```typescript
-   # From the repo's root
-   npm run compile or npx hardhat compile
-   ```
+    ```typescript
+    # From the repo's root
+    npm run compile or npx hardhat compile
+    ```
 
-   [Compiled directory structure](https://hardhat.org/hardhat-runner/docs/advanced/artifacts#directory-structure)
+    [Compiled directory structure](https://hardhat.org/hardhat-runner/docs/advanced/artifacts#directory-structure)
 
 6. Run ts-tests
 
-   ```typescript
-   # From the repo's root
-   npm run test:all or npx hardhat test
-   ```
+    ```typescript
+    # From the repo's root
+    npm run test:all or npx hardhat test
+    ```
 
-   [More tesing contracts details](https://hardhat.org/hardhat-runner/docs/guides/test-contracts)
-
-
+    [More tesing contracts details](https://hardhat.org/hardhat-runner/docs/guides/test-contracts)
--- contracts/Assertions/A20.sol
+++ contracts/Assertions/A20.sol
@@ -18,44 +18,47 @@
 
 pragma solidity ^0.8.8;
 
-import {DynamicAssertion, Identity, HttpHeader} from "../DynamicAssertion.sol";
+import { DynamicAssertion, Identity, HttpHeader } from "../DynamicAssertion.sol";
 
 contract A20 is DynamicAssertion {
-    function execute(Identity[] memory identities, string[] memory /*secrets*/)
-    public
-    override
-    returns (
-        string memory,
-        string memory,
-        string[] memory,
-        string memory,
-        bool
-    )
-    {
-        string
-        memory description = "The user is an early bird user of the IdentityHub EVM version and has generated at least 1 credential during 2023 Aug 14th ~ Aug 21st.";
-        string memory assertion_type = "IDHub EVM Version Early Bird";
-        assertions.push('{ "src": "$has_joined", "op": "==", "dst": "true" }');
-        schema_url = "https://raw.githubusercontent.com/litentry/vc-jsonschema/main/dist/schemas/12-idhub-evm-version-early-bird/1-0-0.json";
-        bool result = false;
+	function execute(
+		Identity[] memory identities,
+		string[] memory /*secrets*/
+	)
+		public
+		override
+		returns (
+			string memory,
+			string memory,
+			string[] memory,
+			string memory,
+			bool
+		)
+	{
+		string
+			memory description = "The user is an early bird user of the IdentityHub EVM version and has generated at least 1 credential during 2023 Aug 14th ~ Aug 21st.";
+		string memory assertion_type = "IDHub EVM Version Early Bird";
+		assertions.push('{ "src": "$has_joined", "op": "==", "dst": "true" }');
+		schema_url = "https://raw.githubusercontent.com/litentry/vc-jsonschema/main/dist/schemas/12-idhub-evm-version-early-bird/1-0-0.json";
+		bool result = false;
 
-        for (uint256 i = 0; i < identities.length; i++) {
-            if (is_web3(identities[i])) {
-                string memory res = toHex(identities[i].value);
+		for (uint256 i = 0; i < identities.length; i++) {
+			if (is_web3(identities[i])) {
+				string memory res = toHex(identities[i].value);
 
-                string memory url = concatenateStrings(
-                    "http://localhost:19527/events/does-user-joined-evm-campaign?account=",
-                    res
-                );
-                string memory jsonPointer = "/hasJoined";
-                HttpHeader[] memory headers = new HttpHeader[](0);
+				string memory url = concatenateStrings(
+					"http://localhost:19527/events/does-user-joined-evm-campaign?account=",
+					res
+				);
+				string memory jsonPointer = "/hasJoined";
+				HttpHeader[] memory headers = new HttpHeader[](0);
 
-                result = GetBool(url, jsonPointer, headers);
-                if (result) {
-                    break;
-                }
-            }
-        }
-        return (description, assertion_type, assertions, schema_url, result);
-    }
+				result = GetBool(url, jsonPointer, headers);
+				if (result) {
+					break;
+				}
+			}
+		}
+		return (description, assertion_type, assertions, schema_url, result);
+	}
 }
