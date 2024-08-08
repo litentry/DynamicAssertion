a66e72f9ddfa39edc206804f0dc3b2efb97fd12b
Verin1005 <daqingchong0809@gmail.com>
2024-08-08 15:40:51 +0800

    copy integration-tests utils from worker

---
+++ integration-tests/common/transactions.ts
@@ -0,0 +1,164 @@
+import { hexToU8a } from '@polkadot/util';
+import type { IntegrationTestContext } from './common-types';
+
+import {
+    AddressOrPair,
+    ApiPromise,
+    ApiTypes,
+    FrameSystemEventRecord,
+    Keyring,
+    SubmittableExtrinsic,
+} from '@litentry/parachain-api';
+
+// for DI-test
+export const subscribeToEventsWithExtHash = async (
+    requestIdentifier: string,
+    context: IntegrationTestContext
+): Promise<FrameSystemEventRecord[]> => {
+    return new Promise<FrameSystemEventRecord[]>((resolve, reject) => {
+        let blocksToScan = 30;
+        /* 
+        WARNING:The unsubscribe function is called inside the Promise callback, which is executed each time a new blockHeader is received. 
+               `unsubscribe` is intended to unsubscribe a blockHeader if certain conditions are met. 
+                If you use await, you will actually wait for this function to finish executing. 
+                However, since it doesn't return a Promise, using await doesn't make sense and can lead to problematic code behaviour.
+                soooooo, don't use await here
+        */
+        const unsubscribe = context.api.rpc.chain.subscribeNewHeads(async (blockHeader) => {
+            const shiftedApi = await context.api.at(blockHeader.hash);
+
+            const allBlockEvents = await shiftedApi.query.system.events();
+            const allExtrinsicEvents = allBlockEvents.filter(({ phase }) => phase.isApplyExtrinsic);
+
+            const matchingEvent = allExtrinsicEvents.filter((eventRecord) => {
+                const eventData = eventRecord.event.data.toHuman();
+                return (
+                    eventData != undefined &&
+                    typeof eventData === 'object' &&
+                    'reqExtHash' in eventData &&
+                    eventData.reqExtHash === requestIdentifier
+                );
+            });
+
+            if (matchingEvent.length == 0) {
+                blocksToScan -= 1;
+                if (blocksToScan < 1) {
+                    reject(new Error(`timed out listening for reqExtHash: ${requestIdentifier} in parachain events`));
+                    (await unsubscribe)();
+                }
+                return;
+            }
+
+            resolve(matchingEvent);
+            (await unsubscribe)();
+        });
+    });
+};
+
+// for II-test
+export const subscribeToEvents = async (
+    section: string,
+    method: string,
+    api: ApiPromise
+): Promise<FrameSystemEventRecord[]> => {
+    return new Promise<FrameSystemEventRecord[]>((resolve, reject) => {
+        let blocksToScan = 15;
+        const unsubscribe = api.rpc.chain.subscribeNewHeads(async (blockHeader) => {
+            const shiftedApi = await api.at(blockHeader.hash);
+
+            const allBlockEvents = await shiftedApi.query.system.events();
+            const allExtrinsicEvents = allBlockEvents.filter(({ phase }) => phase.isApplyExtrinsic);
+
+            const matchingEvent = allExtrinsicEvents.filter(({ event, phase }) => {
+                return event.section === section && event.method === method;
+            });
+
+            if (matchingEvent.length == 0) {
+                blocksToScan -= 1;
+                if (blocksToScan < 1) {
+                    reject(new Error(`timed out listening for event ${section}.${method}`));
+                    (await unsubscribe)();
+                }
+                return;
+            }
+
+            resolve(matchingEvent);
+            (await unsubscribe)();
+        });
+    });
+};
+
+export async function waitForBlock(api: ApiPromise, blockNumber: number, blocksToCheck = 5) {
+    let count = 0;
+
+    return new Promise<void>((resolve, reject) => {
+        const unsubscribe = api.rpc.chain.subscribeNewHeads(async (header) => {
+            console.log(`Chain is at block: #${header.number}`);
+
+            if (header.number.toNumber() === blockNumber) {
+                (await unsubscribe)();
+                resolve();
+            }
+
+            if (++count === blocksToCheck) {
+                (await unsubscribe)();
+                reject(new Error(`Timeout: Block #${blockNumber} not reached within ${blocksToCheck} blocks.`));
+            }
+        });
+    });
+}
+
+export async function setAliceAsAdmin(api: ApiPromise) {
+    // Get keyring of Alice, who is also the sudo in dev chain spec
+    const keyring = new Keyring({ type: 'sr25519' });
+    const alice = keyring.addFromUri('//Alice');
+
+    const tx = await sudoWrapperGC(api, api.tx.teebag.setAdmin('esqZdrqhgH8zy1wqYh1aLKoRyoRWLFbX9M62eKfaTAoK67pJ5'));
+
+    console.log(`Setting Alice as Admin for Teebag`);
+    return signAndSend(tx, alice);
+}
+
+export function signAndSend(tx: SubmittableExtrinsic<ApiTypes>, account: AddressOrPair) {
+    return new Promise<{ block: string }>(async (resolve, reject) => {
+        await tx.signAndSend(account, (result) => {
+            console.log(`Current status is ${result.status}`);
+            if (result.status.isInBlock) {
+                console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
+            } else if (result.status.isFinalized) {
+                console.log(`Transaction finalized at blockHash ${result.status.asFinalized}`);
+                resolve({
+                    block: result.status.asFinalized.toString(),
+                });
+            } else if (result.status.isInvalid) {
+                reject(`Transaction is ${result.status}`);
+            }
+        });
+    });
+}
+
+// After removing the sudo module, we use `EnsureRootOrHalfCouncil` instead of `Sudo`,
+// and there are only two council members in litmus-dev/rococo-dev/litentry-dev.
+// So only `propose` is required, no vote.
+//
+// TODO: support to send the `vote extrinsic`, if the number of council members is greater than 2.
+export async function sudoWrapperGC(api: ApiPromise, tx: SubmittableExtrinsic<ApiTypes>) {
+    const chain = (await api.rpc.system.chain()).toString().toLowerCase();
+    if (chain != 'rococo-dev') {
+        const threshold = api.createType('Compact<u32>', 1);
+        const call = api.createType('Call', tx);
+        return api.tx.council.propose(threshold, call, api.createType('Compact<u32>', tx.length));
+    } else {
+        return api.tx.sudo.sudo(tx);
+    }
+}
+
+export async function setScheduledEnclave(api: ApiPromise, block: number, mrenclave: string) {
+    const keyring = new Keyring({ type: 'sr25519' });
+    const alice = keyring.addFromUri('//Alice');
+
+    const tx = api.tx.teebag.setScheduledEnclave('Identity', block, hexToU8a(`0x${mrenclave}`));
+
+    console.log('Schedule Enclave Extrinsic sent');
+    return signAndSend(tx, alice);
+}
---
+++ integration-tests/common/utils/crypto.ts
@@ -0,0 +1,209 @@
+import type { HexString } from '@polkadot/util/types';
+import { bufferToU8a, hexToU8a, isString, stringToU8a, u8aToHex } from '@polkadot/util';
+import { KeyObject } from 'crypto';
+import { AesOutput, CorePrimitivesIdentity } from '@litentry/parachain-api';
+import crypto from 'crypto';
+import { KeyringPair } from '@polkadot/keyring/types';
+import { ethers } from 'ethers';
+import { blake2AsU8a } from '@polkadot/util-crypto';
+import { Keypair } from '@solana/web3.js';
+import nacl from 'tweetnacl';
+import { IntegrationTestContext } from './../common-types';
+import { buildIdentityHelper } from './identity-helper';
+import { ECPairInterface } from 'ecpair';
+import * as bitcoinMessage from 'bitcoinjs-message';
+import { isHexString } from 'ethers/lib/utils';
+export type KeypairType = 'ed25519' | 'sr25519' | 'ecdsa' | 'ethereum' | 'bitcoin';
+
+export function encryptWithTeeShieldingKey(teeShieldingKey: KeyObject, plaintext: Uint8Array): Buffer {
+    return crypto.publicEncrypt(
+        {
+            key: teeShieldingKey,
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            oaepHash: 'sha256',
+        },
+        plaintext
+    );
+}
+
+// A lazy version without aad. Append the tag to be consistent with rust implementation
+export function encryptWithAes(key: HexString, nonce: Uint8Array, cleartext: Buffer): HexString {
+    const secretKey = crypto.createSecretKey(hexToU8a(key));
+    const cipher = crypto.createCipheriv('aes-256-gcm', secretKey, nonce, {
+        authTagLength: 16,
+    });
+    let encrypted = cipher.update(cleartext.toString('hex'), 'hex', 'hex');
+    encrypted += cipher.final('hex');
+    encrypted += cipher.getAuthTag().toString('hex');
+    return `0x${encrypted}`;
+}
+
+export function decryptWithAes(key: HexString, aesOutput: AesOutput, type: 'hex' | 'utf-8'): HexString {
+    const secretKey = crypto.createSecretKey(hexToU8a(key));
+    const tagSize = 16;
+    const ciphertext = aesOutput.ciphertext ? aesOutput.ciphertext : hexToU8a('0x');
+
+    const nonce = aesOutput.nonce ? aesOutput.nonce : hexToU8a('0x');
+    const aad = aesOutput.aad ? aesOutput.aad : hexToU8a('0x');
+
+    // notice!!! extract author_tag from ciphertext
+    // maybe this code only works with rust aes encryption
+    const authorTag = ciphertext.subarray(ciphertext.length - tagSize);
+
+    const decipher = crypto.createDecipheriv('aes-256-gcm', secretKey, nonce, {
+        authTagLength: tagSize,
+    });
+    decipher.setAAD(aad);
+    decipher.setAuthTag(authorTag);
+
+    const part1 = decipher.update(ciphertext.subarray(0, ciphertext.length - tagSize), undefined, type);
+    const part2 = decipher.final(type);
+
+    return `0x${part1 + part2}`;
+}
+
+export interface Signer {
+    getAddressRaw(): Uint8Array;
+    sign(message: HexString | string | Uint8Array): Promise<Uint8Array>;
+    type(): KeypairType;
+    getAddressInSubstrateFormat(): Uint8Array;
+    getIdentity(api: IntegrationTestContext): Promise<CorePrimitivesIdentity>;
+}
+
+export class PolkadotSigner implements Signer {
+    keypair: KeyringPair;
+
+    constructor(keypair: KeyringPair) {
+        this.keypair = keypair;
+    }
+
+    getAddressRaw(): Uint8Array {
+        return this.keypair.addressRaw;
+    }
+
+    sign(message: HexString | string | Uint8Array): Promise<Uint8Array> {
+        return new Promise((resolve) => resolve(this.keypair.sign(message)));
+    }
+
+    type(): KeypairType {
+        return this.keypair.type;
+    }
+
+    getAddressInSubstrateFormat(): Uint8Array {
+        return this.getAddressRaw();
+    }
+
+    getIdentity(context: IntegrationTestContext): Promise<CorePrimitivesIdentity> {
+        return buildIdentityHelper(u8aToHex(this.getAddressRaw()), 'Substrate', context);
+    }
+}
+
+export class EthersSigner implements Signer {
+    wallet: ethers.Wallet;
+
+    constructor(wallet: ethers.Wallet) {
+        this.wallet = wallet;
+    }
+
+    getAddressRaw(): Uint8Array {
+        return hexToU8a(this.wallet.address);
+    }
+
+    sign(message: HexString | string | Uint8Array): Promise<Uint8Array> {
+        return this.wallet.signMessage(message).then((sig) => {
+            return hexToU8a(sig);
+        });
+    }
+
+    type(): KeypairType {
+        return 'ethereum';
+    }
+
+    getAddressInSubstrateFormat(): Uint8Array {
+        const prefix = stringToU8a('evm:');
+        const address = this.getAddressRaw();
+        const merged = new Uint8Array(prefix.length + address.length);
+        merged.set(prefix);
+        merged.set(address, 4);
+        return blake2AsU8a(merged, 256);
+    }
+
+    getIdentity(context: IntegrationTestContext): Promise<CorePrimitivesIdentity> {
+        return buildIdentityHelper(u8aToHex(this.getAddressRaw()), 'Evm', context);
+    }
+}
+
+export class BitcoinSigner implements Signer {
+    keypair: ECPairInterface;
+
+    constructor(keypair: ECPairInterface) {
+        this.keypair = keypair;
+    }
+
+    getAddressRaw(): Uint8Array {
+        return bufferToU8a(this.keypair.publicKey);
+    }
+
+    sign(message: HexString | string | Uint8Array): Promise<Uint8Array> {
+        return new Promise((resolve, reject) => {
+            if (isString(message)) {
+                // produce deterministic signatures
+                const sig = bitcoinMessage.sign(message, this.keypair.privateKey!, this.keypair.compressed);
+                resolve(sig);
+            } else {
+                reject('wrong message type');
+            }
+        });
+    }
+
+    type(): KeypairType {
+        return 'bitcoin';
+    }
+
+    getAddressInSubstrateFormat(): Uint8Array {
+        return blake2AsU8a(this.getAddressRaw(), 256);
+    }
+
+    getIdentity(context: IntegrationTestContext): Promise<CorePrimitivesIdentity> {
+        return buildIdentityHelper(u8aToHex(this.getAddressRaw()), 'Bitcoin', context);
+    }
+}
+
+export class SolanaSigner implements Signer {
+    keypair: Keypair;
+
+    constructor(keypair: Keypair) {
+        this.keypair = keypair;
+    }
+
+    getAddressRaw(): Uint8Array {
+        return this.keypair.publicKey.toBytes();
+    }
+
+    sign(message: HexString | string | Uint8Array): Promise<Uint8Array> {
+        return new Promise((resolve) =>
+            resolve(
+                nacl.sign.detached(
+                    isHexString(message)
+                        ? hexToU8a(message as HexString)
+                        : isString(message)
+                        ? stringToU8a(message)
+                        : message,
+                    this.keypair.secretKey
+                )
+            )
+        );
+    }
+
+    type(): KeypairType {
+        return 'ed25519';
+    }
+
+    getAddressInSubstrateFormat(): Uint8Array {
+        return this.getAddressRaw();
+    }
+
+    getIdentity(context: IntegrationTestContext): Promise<CorePrimitivesIdentity> {
+        return buildIdentityHelper(u8aToHex(this.getAddressRaw()), 'Solana', context);
+    }
+}
