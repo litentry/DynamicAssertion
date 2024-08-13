cc8e3b0b00a5719c27d46a636bfdf68a18804ceb
Verin1005 <daqingchong0809@gmail.com>
2024-08-13 15:12:06 +0800

    --clean-reset

--- scripts/run_parachain_worker.sh
+++ scripts/run_parachain_worker.sh
@@ -50,12 +50,12 @@ function restart_worker_services {
     commands=(
       "-T ws:localhost -P 2000 -p 9944 -r 3443 -w 2001 -h 4545 --enable-metrics --parentchain-start-block 947410 run --skip-ra --dev"
     )
-  else 
+  else
     # staging example
     commands=(
-      "-T ws:localhost -P 2000 -p 9944 -r 3443 -w 2001 -h 4545 --parentchain-start-block 0 run --skip-ra --dev"
+      "--clean-reset -T ws:localhost -P 2000 -p 9944 -r 3443 -w 2001 -h 4545 --parentchain-start-block 0 run --skip-ra --dev"
     )
-  fi 
+  fi
   docker run -d \
       --name litentry-worker-${WORKER_ID} \
       --restart always \
@@ -65,8 +65,6 @@ function restart_worker_services {
       --env RUST_LOG=info,litentry_worker=debug,ws=warn,sp_io=error,substrate_api_client=warn,itc_parentchain_light_client=info,jsonrpsee_ws_client=warn,jsonrpsee_ws_server=warn,enclave_runtime=debug,ita_stf=debug,its_rpc_handler=warn,itc_rpc_client=warn,its_consensus_common=debug,its_state=warn,its_consensus_aura=warn,aura*=warn,its_consensus_slots=warn,itp_attestation_handler=debug,http_req=debug,lc_mock_server=warn,itc_rest_client=debug,lc_credentials=debug,lc_identity_verification=debug,lc_stf_task_receiver=debug,lc_stf_task_sender=debug,lc_data_providers=debug,itp_top_pool=debug,itc_parentchain_indirect_calls_executor=debug,bc_task_receiver=debug \
       --env DATA_DIR='/data' \
       --env-file /opt/worker_configs/worker_env \
-      --volume ${WORKER_BASEDIR}/w${WORKER_ID}:/data \
-      --workdir /data \
       litentry/identity-worker:${WORKER_TAG} ${commands[${WORKER_ID}]}
 }
 
